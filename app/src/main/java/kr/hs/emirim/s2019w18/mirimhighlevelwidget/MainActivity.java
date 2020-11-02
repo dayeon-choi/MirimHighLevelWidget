863d4f78dee6e7243363d78d9e9cc12fef655b7a
dayeon <dayeon2399@naver.com>
2020-11-02 20:06:20 +0900

    dayeon's commit - reservation2 done:)

--- app/src/main/java/kr/hs/emirim/s2019w18/mirimhighlevelwidget/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019w18/mirimhighlevelwidget/MainActivity.java
@@ -10,15 +10,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.Chronometer;
+import android.widget.DatePicker;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
-import javax.xml.transform.Result;
-
 public class MainActivity extends AppCompatActivity {
     RadioButton radioDate,radioTime;
-    CalendarView calendar1;
+    DatePicker datePicker;
     TimePicker timePicker;
     TextView textResult;
     Chronometer chrono;
@@ -26,16 +25,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reservation);
+        setContentView(R.layout.activity_reservation2);
         radioDate = findViewById(R.id.radio_date);
         radioTime = findViewById(R.id.radio_time);
         radioDate.setOnClickListener(radioListener);
         radioTime.setOnClickListener(radioListener);
-        calendar1=findViewById(R.id.calendar1);
+        datePicker=findViewById(R.id.date_pick);
         timePicker=findViewById(R.id.time_pick);
         chrono=findViewById(R.id.chrono1);
+        chrono.setOnClickListener(chronoListener);
         textResult=findViewById(R.id.text_result);
-        Button btnStart=findViewById(R.id.btn_start);
+        textResult.setOnLongClickListener(textListener);
     }
 
     View.OnClickListener radioListener = new View.OnClickListener(){
@@ -43,37 +43,30 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             switch (v.getId()){
                 case R.id.radio_date:
-                    calendar1.setVisibility(View.VISIBLE);
+                    datePicker.setVisibility(View.VISIBLE);
                     timePicker.setVisibility(View.INVISIBLE);
                     break;
                 case R.id.radio_time:
-                    calendar1.setVisibility(View.INVISIBLE);
+                    datePicker.setVisibility(View.INVISIBLE);
                     timePicker.setVisibility(View.VISIBLE);
                     break;
             }
         }
     };
-    View.OnClickListener btnListener=new View.OnClickListener(){
+    View.OnClickListener chronoListener =new View.OnClickListener(){
         public void onClick(View v){
-            switch(v.getId()){
-                case R.id.btn_start:
-                    chrono.setBase(SystemClock.elapsedRealtime());
-                    chrono.start();
-                    chrono.setTextColor(Color.RED);
-                    break;
-                case R.id.btn_stop:
-                    chrono.stop();
-                    chrono.setTextColor(Color.BLUE);
-                    textResult.setText(dateStr+timePicker.getCurrentHour()+"시 "+timePicker.getCurrentMinute()+"분");
-                    break;
+            chrono.setBase(SystemClock.elapsedRealtime());
+            chrono.start();
+            chrono.setTextColor(Color.RED);
             }
-        }
-    };
-    String dateStr="";
-    CalendarView.OnDateChangeListener calendarListener=new CalendarView.OnDateChangeListener(){
-        @Override
-        public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfMonth) {
-            dateStr=year+"년 "+(month+1)+"월 "+dayOfMonth+"일 ";
+        };
+
+    View.OnLongClickListener textListener=new View.OnLongClickListener(){
+        public boolean onLongClick(View v){
+            chrono.stop();
+            chrono.setTextColor(Color.BLUE);
+            textResult.setText(datePicker.getYear()+"년 "+(datePicker.getMonth()+1)+"월 "+timePicker.getCurrentHour()+"시 "+timePicker.getCurrentMinute()+"분");
+            return false;//기본행동 취소
         }
     };
 }
